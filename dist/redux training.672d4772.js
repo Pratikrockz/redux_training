609b204577d4aec920fa048e54c9303966536405
Pratik Pahari <90968509+Pratikrockz@users.noreply.github.com>
2026-02-12 23:05:58 +0530

    Initial commit

---
+++ dist/redux training.672d4772.js
@@ -0,0 +1,7 @@
+let reduxStore = {
+    post: 0,
+    name: "pratik",
+    age: 28
+};
+
+//# sourceMappingURL=redux training.672d4772.js.map
---
+++ script.js
@@ -0,0 +1,65 @@
+import { createStore } from "redux"
+import { productList } from "./productList"
+
+
+
+
+// const initialState = {
+//     post: 0,
+//     name: "pratik",
+//     age: 28
+// }
+
+const initialState = {
+    products: productList,
+    cartItem: [
+    ],
+    wishList: []
+}
+
+const CART_ADD_ITEM='cartItem/add'
+const CART_WISHLIST_ADD='cartItem/wishList'
+const CART_DELETE_ITEM='cartItem/delete'
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case CART_ADD_ITEM:
+
+            return { ...state, cartItem: [...state.cartItem, action.payload] };
+        case CART_WISHLIST_ADD:
+
+            return { ...state, wishList: [...state.wishList, action.payload] };
+        case CART_DELETE_ITEM:
+            return {...state,cartItem:state.cartItem.filter((a)=>
+               
+                a.productId !== action.payload.productId
+            )}
+
+        default:
+            return state;
+    }
+
+}
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.())
+
+store.subscribe(() => {
+    console.log(store.getState())
+})
+
+store.dispatch({ type: CART_ADD_ITEM, payload: { productId: 1, quantity: 1 } })
+store.dispatch({ type: CART_ADD_ITEM, payload: { productId: 5, quantity: 1 } })
+store.dispatch({ type: CART_ADD_ITEM, payload: { productId: 8, quantity: 1 } })
+store.dispatch({ type: CART_ADD_ITEM, payload: { productId: 9, quantity: 1 } })
+
+//store.dispatch({ type: CART_WISHLIST_ADD, payload: { productId: 5, quantity: 1 } })
+
+store.dispatch({ type: CART_DELETE_ITEM, payload: { productId: 5} })
+
+
+
+
+
+
+
+
